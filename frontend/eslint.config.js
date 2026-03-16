9e17a84b268ddfff0f6ebf20bc43beb2d02a91b7
Ompradeep <ompradeep2004@gmail.com>
2026-03-17 00:26:59 +0530

    feat: Add security linting for frontend and backend, and integrate secret, SAST, SCA, and container vulnerability scans into the CI/CD pipeline.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import securityPlugin from 'eslint-plugin-security'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -12,6 +13,7 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      securityPlugin.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
